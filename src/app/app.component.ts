94b865c1e1edb32f0ddd0e1e60ef1e99d945bbf8
jhbui1 <jhbui1@uci.edu>
2020-04-14 15:02:50 -0500

    map api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { AgmCoreModule } from '@agm/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'co-op19';
+  title = 'angulargooglemap';
+  lat: number = 43.653908;
+  lng: number = -79.384293;
 }
