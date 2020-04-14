cfc35f1f9747f17e62e07fcaab7b9e4b81f618da
Jonathan Bui <30819087+jhbui1@users.noreply.github.com>
2020-04-13 23:42:10 -0500

    Update SQLQuery.sql
    -added constraint on mapdata that checks either coords are not null or address is not null to avoid forcing not null on both coordinates and address
    - added tables following proposal name scheme (generic, consumable,health)

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,15 +1,17 @@
 
 create table MapData(
 	ID int identity(1,1) primary key,
-	GPSN decimal(9,6) not null,
-	GPSW decimal(9,6) not null,
-	City nchar(30) not null,
+	GPSN decimal(9,6),
+	GPSW decimal(9,6),
+	City nchar(30),
 	Address nchar(60),
-	State nchar(40) not null
+	State nchar(40),
+  CONSTRAINT chk_location CHECK((GPSN IS NOT NULL AND GPSW IS NOT NULL) OR (CITY IS NOT NULL AND STATE IS NOT NULL)) 
 );
 
 create table Users(
-	ID int primary key foreign key references MapData(ID),
+	ID int primary key 
+  Loc INT foreign key references MapData(ID),
 	UserName nchar(30) unique not null,
 	Password nchar(30) not null,
 	FName nchar(30) not null,
@@ -19,27 +21,45 @@ create table Users(
 );
 
 create table Locations(
-	ID int primary key foreign key references MapData(ID),
+	ID int IDENTITY primary key foreign key references MapData(ID),
 	Title nchar(40) unique not null,
 	Comment nchar(500)
 );
 
-create table Items(
-	ID int identity(1,1) primary key,
-	LocID int foreign key references MapData(ID),
-	Name nchar(30) not null,
-	Price money,
-	Availibility tinyint not null
+CREATE TABLE GenericResource(
+  ID INT IDENTITY PRIMARY KEY,
+  Name VARCHAR(40),
+  Description VARCHAR(40),
+  LocID int foreign key references MapData(ID)
 );
 
-create table CareCenter(
-	ID int primary key foreign key references MapData(ID),
-	ProvidesTests bit not null,
+create table ConsumableResource(
+	ID int identity(1,1) primary key,
+  Resource_ID FOREIGN KEY REFERENCES GenericResource(ID) not null,
+  Price MONEY,
+  Quantity INT NOT NULL CHECK (Quantity>0)
+);
+
+create table HealthResource(
+	ID int IDENTITY primary key,
+  Resource_ID FOREIGN KEY REFERENCES GenericResource(ID) not null,
+	ProvidesTests bit not null DEFAULT false,
 	TestPrice money
 );
 
-create table Zone(
-	ID int primary key foreign key references MapData(ID),
-	Rating tinyint not null,
+CREATE TABLE HealthResourceServices(
+  ID INT IDENTITY PRIMARY KEY,
+  ServiceName VARCHAR(50) NOT NULL,
+  ServiceDesc VARCHAR(50) NOT NULL,
+  AvgWaitHours INT,
+  EstCost Money NOT NULL DEFAULT 0
+);
+
+CREATE TABLE ShelterResource(
+  ID INT IDENTITY PRIMARY KEY,
+  Resource_ID FOREIGN KEY REFERENCES GenericResource(ID) not null,
+  Vacancy INT NOT NULL CHECK (Vacancy>0),
+  Rating tinyint not null,
 	IsSafe bit not null
 );
+  
