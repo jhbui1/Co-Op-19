3b6b232e30dc7598f8325ba1881b641f6e553cbb
jhbui1 <jhbui1@uci.edu>
2020-04-16 21:30:09 -0500

    Register form

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,11 @@
+export class User {
+    constructor(
+        public Username : string,
+        public Password : string,
+        public FName : string,
+        public LName : string,
+        public Phone : string,
+        public Email : string
+    ) {}
+    
+}
